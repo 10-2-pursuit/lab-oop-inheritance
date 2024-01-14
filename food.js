d5bf3b76c060416a20362b35802753b1107f9b1f
Nicole Slater <nicoleslater@pursuit.org>
2024-01-14 15:28:18 -0500

    Updating isFresh, Prepare, aDayPasses

--- food.js
+++ food.js
@@ -5,7 +5,33 @@ class Food {
         this.daysToSpoil = daysToSpoil;
         this.fresh = fresh;
     } 
+
+    prepare(){
+        console.log(`${this.name} is being prepared.`);
+    }
+
+    isFresh(){
+        if(this.fresh && this.daysToSpoil > 0){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+        } else {
+            console.log(`${this.name} has spoiled. Please Discard Immediatelly!`);
+        }
+    }
+
+    aDayPasses(){
+        this.daysToSpoil -= 1;
+        this.isFresh();
+    }
 }
+
+const avocado = new Food("Avocado", 3);
+
+avocado.prepare();
+avocado.isFresh();
+avocado.aDayPasses();
+
+
+
 
 // Do not edit below this line
 module.exports = Food;
