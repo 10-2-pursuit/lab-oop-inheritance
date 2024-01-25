6485e7c8e43fb55ba1f612ca1bc56c9524d1187c
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-25 18:42:06 -0500

    builds sleep functionality

--- tamagotchi.js
+++ tamagotchi.js
@@ -62,6 +62,10 @@ class Tamagotchi {
             this.full--
         }
     }
+    sleep() {
+        this.energy+=4;
+        this.full-=3
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
