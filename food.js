0d21a9e154c7869dc7634b541d56b741b9ed5f84
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-25 18:33:42 -0500

    builds out more of tamagotchi class

--- food.js
+++ food.js
@@ -2,23 +2,23 @@
 class Food {
     constructor(name, daysToSpoil, fresh = true) {
         this.name = name;
-        this.daysRoSpoil = daysToSpoil;
+        this.daysToSpoil = daysToSpoil;
         this.fresh = fresh
     }
     prepare() {
-        console.log(`${this.name} is being prepared.`)
+        console.log(`${this.name} is being prepared`)
     }
     isFresh() {
         if(this.fresh == false || this.daysToSpoil <= 0) {
-            console.log(`${this.name} has gone bad.`) 
+            console.log(`${this.name} has spoiled.`) 
         } else {
-            console.log(`There are ${this.daysToSpoil} days left before ${this.name} goes bad.`)
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
         }
+    }
         aDayPasses() {
             this.daysToSpoil--
             this.isFresh();
         }
-    }
 }
 // Do not edit below this line
 module.exports = Food;
