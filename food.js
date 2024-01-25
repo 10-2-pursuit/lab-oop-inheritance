8b9747b413cb0d3695e3c1bf4e1f16c73ca62778
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2024-01-25 18:11:09 -0500

    gets started on lab

--- food.js
+++ food.js
@@ -1,4 +1,18 @@
 // Create class below
-
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysRoSpoil = daysToSpoil;
+        this.fresh = fresh
+    }
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh() {
+        if(this.fresh == false || this.daysToSpoil <= 0) {
+            console.log(`${}`)
+        }
+    }
+}
 // Do not edit below this line
 module.exports = Food;
